d052d663ae81c6e5ecab25a2d214f751ab26b246
Mensur Vukovic <mensur.v@hotmail.com>
2018-03-02 12:49:23 +0100

    Odradjeno sa file-om

--- src/Banka.java
+++ src/Banka.java
@@ -1,15 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Banka {
 
 	static Scanner unos = new Scanner(System.in);
 	private static Racun racun;
+	static int izbor, brojRacuna, sourceAcc, targetAcc;
+	static int brojac = 0;
+	static double iznosNaRacunu, iznos;
 
-	public static void main(String[] args) {
 
-		System.out.println("Izaberite opciju: ");
-		System.out.println("\n1. Kreiranje raèuna \n2. Prebacivanje novca \n3. Ispisivanje raèuna ");
-		int izbor = unos.nextInt();
+	public static void main(String[] args) throws IOException {
+
+		if (brojac == 0) {
+			citaj();
+			brojac++;
+		}
+
+		boolean nastavak = true;
+
+		do {
+			try {
+
+				System.out.println("Izaberite opciju: ");
+				System.out.println(
+						"\n1. Kreiranje raèuna \n2. Prebacivanje novca \n3. Ispisivanje raèuna \n4. Exit&Save ");
+				izbor = unos.nextInt();
+
+				if (izbor < 1 || izbor > 4) {
+					nastavak = true;
+					System.err.println("Pogresan unos, unesite brojeve izmedju 1 - 4!");
+					continue;
+				}
+
+				nastavak = false;
+
+			} catch (InputMismatchException e) {
+				System.err.println("Pogresan unos, unesite brojeve izmedju 1 - 4!");
+				unos.nextLine();
+			}
+		} while (nastavak);
 
 		switch (izbor) {
 
@@ -33,28 +69,49 @@ public class Banka {
 			Banka.main(args);
 
 			break;
+		case 4:
+			upisi();
+			System.exit(0);
 		}
 
 	}
 
-	private static void kreiranjeRacuna() {
+	private static void kreiranjeRacuna() throws IOException {
 
-		System.out.println("Unesite broj raèuna: ");
-		int brojRacuna = unos.nextInt();
-
+		boolean nastavak = true;
+		do {
+			try {
+				System.out.println("Unesite broj raèuna: ");
+				brojRacuna = unos.nextInt();
+				nastavak = false;
+			} catch (InputMismatchException e) {
+				System.err.print("Pogresan unos!");
+				unos.nextLine();
+			}
+		} while (nastavak);
 		while (provjeraRacuna(brojRacuna)) {
-			System.out.println("Broj raèuna vec postoji ili je negativan broj, pokušajte sa drugim brojem: ");
+			System.err.println("Broj raèuna vec postoji ili je negativan broj, pokušajte sa drugim brojem: ");
 			brojRacuna = unos.nextInt();
 		}
 
 		System.out.println("Unesite ime korisnika: ");
 		String ime = unos.next();
 
-		System.out.println("Unesite iznos na raèunu: ");
-		double iznosNaRacunu = unos.nextInt();
+		nastavak = true;
+
+		do {
+			try {
+				System.out.println("Unesite iznos na raèunu: ");
+				iznosNaRacunu = unos.nextDouble();
+				nastavak = false;
+			} catch (InputMismatchException e) {
+				System.err.print("Pogresan unos!");
+				unos.nextLine();
+			}
+		} while (nastavak);
 
 		while (provjeraIznosa(iznosNaRacunu)) {
-			System.out.println("Unijeli ste negativan iznos, unesite ponovno:");
+			System.err.println("Unijeli ste negativan iznos, unesite ponovno:");
 			iznosNaRacunu = unos.nextDouble();
 		}
 
@@ -113,24 +170,54 @@ public class Banka {
 
 	private static void transakcija() {
 
-		System.out.println("Unesite broj raèuna sa kojeg prebacujete novac: ");
-		int sourceAcc = unos.nextInt();
+		boolean nastavak = true;
+
+		do {
+			try {
+				System.out.println("Unesite broj raèuna sa kojeg prebacujete novac: ");
+				sourceAcc = unos.nextInt();
+				nastavak = false;
+			} catch (InputMismatchException e) {
+				System.err.print("Pogresan unos!");
+				unos.nextLine();
+			}
+		} while (nastavak);
 
 		while (!provjeraBrojaRacuna(sourceAcc)) {
 			System.out.println("Unešeni raèun ne postoji, provjerite raèun i pokušajte ponovno: ");
 			sourceAcc = unos.nextInt();
 		}
 
-		System.out.println("Unesite broj raèuna na koji prebacujete novac: ");
-		int targetAcc = unos.nextInt();
+		nastavak = true;
+
+		do {
+			try {
+				System.out.println("Unesite broj raèuna na koji prebacujete novac: ");
+				targetAcc = unos.nextInt();
+				nastavak = false; 
+			} catch (InputMismatchException e) {
+				System.err.print("Pogresan unos!");
+				unos.nextLine();
+			}
+		} while (nastavak);
 
 		while (!provjeraBrojaRacuna(targetAcc)) {
 			System.out.println("Unešeni raèun ne postoji, provjerite raèun i pokušajte ponovno: ");
 			sourceAcc = unos.nextInt();
 		}
 
-		System.out.println("Unesite iznos novca koji želite prebaciti: ");
-		double iznos = unos.nextDouble();
+		nastavak = true;
+
+		do {
+			try {
+				System.out.println("Unesite iznos novca koji želite prebaciti: ");
+				iznos = unos.nextDouble();
+				nastavak = false;
+			} catch (InputMismatchException e) {
+				System.err.print("Pogresan unos!");
+				unos.nextLine();
+			}
+		} while (nastavak);
 
 		int indexS = indexSource(sourceAcc);
 		int indexT = indexTarget(targetAcc);
@@ -155,5 +242,36 @@ public class Banka {
 
 	public static void setRacun(Racun racun) {
 		Banka.racun = racun;
+	}
+
+	public static void upisi() throws IOException {
+
+		Path path = Paths.get("Racuni.txt");
+		BufferedWriter writer = Files.newBufferedWriter(path);
+
+		for (Racun e : Racun.getRacuni()) {
+			writer.write(e.getBrojRacuna() + " ");
+			writer.write(e.getIme() + " ");
+			writer.write(String.valueOf(e.getIznosNaRacunu()));
+			writer.newLine();
+
+		}
+		writer.close();
+
+	}
+
+	public static void citaj() throws IOException {
+
+		Path path = Paths.get("Racuni.txt");
+		BufferedReader reader = Files.newBufferedReader(path);
+		String line;
+
+		while ((line = reader.readLine()) != null) {
+			String[] var = line.split(" ");
+
+			racun = new Racun(Integer.parseInt(var[0]), var[1], Double.parseDouble(var[2]));
+
+		}
+		reader.close();
 	}
 }
